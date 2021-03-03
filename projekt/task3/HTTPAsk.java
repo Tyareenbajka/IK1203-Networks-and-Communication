5fb58cec52b57cb9196995ee4923ff6f6a1b893c
Tyareenbajka <58476139+Tyareenbajka@users.noreply.github.com>
2021-03-03 18:11:09 +0100

    update

---
+++ projekt/task3/HTTPAsk.java
@@ -0,0 +1,61 @@
+import tcpclient.TCPClient;
+
+import java.net.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class HTTPAsk {
+    static int BUFFERSIZE = 1024;
+    public static void main( String[] args) throws IOException {
+        byte[] fromClientBuffer = new byte[BUFFERSIZE];
+        byte[] toClientBuffer = new byte[BUFFERSIZE];
+        ServerSocket welcomeSocket = new ServerSocket(Integer.parseInt(args[0]));
+
+        int portFromClient = 0;
+        String host = null;
+        String stringFromClient = null;
+        String result = null;
+        String statusMsg = null;
+
+        while(true){
+            Socket connectionSocket = welcomeSocket.accept();
+            InputStream inFromClient = connectionSocket.getInputStream();
+            OutputStream outToClient = connectionSocket.getOutputStream();
+            StringBuilder outToClientData = new StringBuilder();
+            String decodeString = "";
+
+            int charChecker = inFromClient.read(fromClientBuffer);
+            while(!decodeString.contains("\n")){
+                decodeString = new String(fromClientBuffer, 0, charChecker);
+                String[] splitString = decodeString.split("[?&= ]", 10);
+
+                if(splitString[0].equals("GET") && splitString[1].equals("/ask") && decodeString.contains("HTTP/1.1")){
+                    statusMsg = ("HTTP/1.1 200 OK \r\n\r\n");
+                }
+                else{
+                    statusMsg = ("HTTP/1.1 400 Bad Request \r\n\r\n");
+                }
+                for(int i = 0; i < splitString.length; i++){
+                    if(splitString[i].equals("hostname"))
+                        host = splitString[i+1];
+                    else if(splitString[i].equals("port"))
+                        portFromClient = Integer.parseInt(splitString[i+1]);
+                    else if(splitString[i].equals("string"))
+                        stringFromClient = splitString[i+1];
+                }
+            }
+            if(stringFromClient != null){
+                result = TCPClient.askServer(host, portFromClient, stringFromClient);
+            }
+            else{
+                result = TCPClient.askServer(host, portFromClient);
+            }
+            outToClientData.append(statusMsg + result + "\r\n");
+            byte [] sendDataToClient = outToClientData.toString().getBytes();
+            outToClient.write(sendDataToClient);
+            connectionSocket.close();
+
+        }
+    }
+}
+
